ff668724e05d53ee087264e08733fe345b1b7db5
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-10 09:23:47 +0530

    Add:UC1 added

--- src/com/company/bridegelabz/tictacto/TicTacToe.java
+++ src/com/company/bridegelabz/tictacto/TicTacToe.java
@@ -2,9 +2,25 @@ package com.company.bridegelabz.tictacto;
 
 public class TicTacToe
 {
+    public static char board[][];
+    public static void printMatrix()
+    {
+        board=new char[4][4];
+        for(int i=1;i<board.length;i++)
+        {
+            for(int j=1;j<4;j++)
+            {
+                System.out.print("___"+board[i][j]+"|");
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] args)
     {
         System.out.println("Welcome");
+        printMatrix();
 
     }
 }
