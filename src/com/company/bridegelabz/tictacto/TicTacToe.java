88e407ebe679349081d0e9b7317c030724f816ca
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-10 11:24:11 +0530

    Add:UC4-Ask User For Input and Place in structure

--- src/com/company/bridegelabz/tictacto/TicTacToe.java
+++ src/com/company/bridegelabz/tictacto/TicTacToe.java
@@ -2,7 +2,7 @@ package com.company.bridegelabz.tictacto;
 import  java.util.*;
 public class TicTacToe
 {
-    public static char board[];
+    public static char board[]=new char[10];
     public static char[] printMatrix()
     {
         board=new char[10];
@@ -22,15 +22,15 @@ public class TicTacToe
     }
     public static void displayBoard()
     {
-        int k=0;
-        for(int i=0;i<3;i++)
+        int k=1;
+        for(int i=1;i<=3;i++)
         {
-            for(int j=0;j<3;j++)
+            for(int j=1;j<=3;j++)
             {
-
+                //System.out.print(k);
                 System.out.print(board[k]);
-                k++;
-                if(j<2)
+                  k++;
+                if(j<3)
                 {
                     System.out.print(" | ");
                 }
@@ -43,6 +43,27 @@ public class TicTacToe
         }
         System.out.println("  |   |   ");
     }
+      public static void moveLocation()
+     {
+         Scanner scan=new Scanner(System.in);
+         System.out.println("At which position you want to insert value ");
+         int position= scan.nextInt();
+         System.out.println("Enter value ");
+         char output=scan.next().charAt(0);
+         if(board[position]!='X'&&board[position]!='O')
+         {
+             TicTacToe.board[position]=output;
+             displayBoard();
+
+         }
+         else
+         {
+             System.out.println("Position is already filled choose again");
+             moveLocation();
+
+         }
+
+     }
     public static void main(String[] args)
     {
         char computer,user;
@@ -53,5 +74,7 @@ public class TicTacToe
         char computerLetter=(userLetter=='X')?'0':'X';
         System.out.println("computerLetter"+computerLetter);
         displayBoard();
+        //UC4
+        moveLocation();
     }
 }
